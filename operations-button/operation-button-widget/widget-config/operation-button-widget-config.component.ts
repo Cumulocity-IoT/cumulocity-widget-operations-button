87d7a8e88fc23c6b4d6df45790355099249e91e0
ChrisMey <cmey@softwareag.com>
2022-12-14 13:42:10 +0100

    draft

--- operations-button/operation-button-widget/widget-config/operation-button-widget-config.component.ts
+++ operations-button/operation-button-widget/widget-config/operation-button-widget-config.component.ts
@@ -1,25 +1,34 @@
-import { Component, Input } from '@angular/core';
-import { ICONS } from '@c8y/ngx-components';
-import { IOperationButtonWidgetConfig } from '../models/IOperationButtonWidgetConfig';
+import { Component, Input } from "@angular/core";
+import { ICONS } from "@c8y/ngx-components";
+import { IOperationButtonWidgetConfig } from "../models/IOperationButtonWidgetConfig";
 
 @Component({
-  selector: 'app-operation-button-widget-config',
-  templateUrl: './operation-button-widget-config.component.html',
+  selector: "app-operation-button-widget-config",
+  templateUrl: "./operation-button-widget-config.component.html",
 })
 export class OperationButtonWidgetConfigComponent {
+  public supportedOperations: string[] = [];
+  public chosenOperation: string = "";
+
   @Input() config: IOperationButtonWidgetConfig = {};
   buttonClasses = [
-    'btn-default',
-    'btn-primary',
-    'btn-secondary',
-    'btn-success',
-    'btn-danger',
-    'btn-emphasis',
-    'btn-info',
-    'btn-warning',
-    'btn-link',
+    "btn-default",
+    "btn-primary",
+    "btn-secondary",
+    "btn-success",
+    "btn-danger",
+    "btn-emphasis",
+    "btn-info",
+    "btn-warning",
+    "btn-link",
   ];
-  availableIcons: string[] = ['play', 'stop', 'rotate-left', 'rotate-right', ...ICONS];
+  availableIcons: string[] = [
+    "play",
+    "stop",
+    "rotate-left",
+    "rotate-right",
+    ...ICONS,
+  ];
 
   addNewButton(): void {
     if (!this.config.buttons) {
@@ -27,13 +36,17 @@ export class OperationButtonWidgetConfigComponent {
     }
 
     this.config.buttons.push({
-      icon: 'refresh',
-      label: 'Restart',
-      description: 'Restart device',
-      operationFragment: 'c8y_Restart',
-      buttonClasses: 'btn-warning',
-      operationValue: '{}',
+      icon: "refresh",
+      label: "Restart",
+      description: "Restart device",
+      operationFragment: "c8y_Restart",
+      buttonClasses: "btn-warning",
+      operationValue: "{}",
     });
+    
+    if (this.config.device && this.config.device["c8y_SupportedOperations"]) {
+      this.supportedOperations = this.config.device["c8y_SupportedOperations"];
+    }
   }
 
   removeButton(index: number): void {
