7c34d28127fd4a688720584156829f57de861a2c
ck-c8y <christof.strack@cumulocity.com>
2025-10-21 12:27:53 +0200

    add key

--- cumulocity.config.ts
+++ cumulocity.config.ts
@@ -7,6 +7,7 @@ export default {
     description,
     version,
     name,
+      key: 'c8y-pkg-widget-operation-button-key',
     contentSecurityPolicy:
       "base-uri 'none'; default-src 'self' 'unsafe-inline' http: https: ws: wss:; connect-src 'self' http: https: ws: wss:;  script-src 'self' *.bugherd.com *.twitter.com *.twimg.com *.aptrinsic.com 'unsafe-inline' 'unsafe-eval' data:; style-src * 'unsafe-inline' blob:; img-src * data: blob:; font-src * data:; frame-src *; worker-src 'self' blob:;",
     dynamicOptionsUrl: true,
